50b4c03060a3a9531341896d27e7e86d01c2eccb
jonmelo28 <jonmelo28@gmail.com>
2023-09-27 16:39:48 -0300

    second commit

---
+++ resources/views/layout/autentication/footer.blade.php
@@ -0,0 +1,22 @@
+
+<body>
+    
+    @include('layout.autentication.menu')
+    <HR></HR>
+    <nav class="nav nav-tabs" id="nav-tab" role="tablist">
+        <button class="nav-link active" id="nav-model-tab" data-bs_toggle="tab" data-bs-target="#nav-model" type="button" role="tab" aria-controls="nav-model" aria-selected="true">Modelos</button>
+        <button class="nav-link active" id="nav-background-tab" data-bs_toggle="tab" data-bs-target="#nav-background" type="button" role="tab" aria-controls="nav-background" aria-selected="false">Fundo do Cartão</button>
+        <button class="nav-link active" id="nav-logo-tab" data-bs_toggle="tab" data-bs-target="#nav-logo" type="button" role="tab" aria-controls="nav-logo" aria-selected="false">Logotipo</button>
+        <button class="nav-link active" id="nav-highlight-tab" data-bs_toggle="tab" data-bs-target="#nav-highlight" type="button" role="tab" aria-controls="nav-highlight" aria-selected="false">Destaque</button>
+        <button class="nav-link active" id="nav-impact-phrase-tab" data-bs_toggle="tab" data-bs-target="#nav-impact-phrase" type="button" role="tab" aria-controls="nav-impact-phrase" aria-selected="false">Frase de Impacto</button>
+        <button class="nav-link active" id="nav-buttons-tab" data-bs_toggle="tab" data-bs-target="#nav-buttons" type="button" role="tab" aria-controls="nav-buttons" aria-selected="false">Botões</button>
+        <button class="nav-link active" id="nav-catalog-tab" data-bs_toggle="tab" data-bs-target="#nav-catalog" type="button" role="tab" aria-controls="nav-catalog" aria-selected="false">Catálogo</button>
+        <button class="nav-link active" id="nav-media-tab" data-bs_toggle="tab" data-bs-target="#nav-media" type="button" role="tab" aria-controls="nav-media" aria-selected="false">Videos e Áudio</button>
+        <button class="nav-link active" id="nav-banner-tab" data-bs_toggle="tab" data-bs-target="#nav-banner" type="button" role="tab" aria-controls="nav-banner" aria-selected="false">Banner</button>
+        <button class="nav-link active" id="nav-custom-code-tab" data-bs_toggle="tab" data-bs-target="#nav-custom-code" type="button" role="tab" aria-controls="nav-custom-code" aria-selected="false">Código Personalizado</button>
+
+    </nav>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/layout/autentication/menu.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active">Projetos</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Tutorias</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Perfil</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Suporte</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Sair</a></li>
+    </ul>
+</header>
